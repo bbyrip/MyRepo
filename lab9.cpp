7301a92b727c1663e3ddf9be1cd62a19d2742adf
Nikita <riptrudmay@mail.ru>
2024-06-01 15:51:03 +0300

    new commit in main branch

--- lab9.cpp
+++ lab9.cpp
@@ -6,6 +6,9 @@ int main()
 {
     //Comment
     cout << "Hello World!1" << endl;
+    //new comment
     cout << "Hello World!2" << endl;
+    cout << "Hello World!5" << endl;
+    cout << "Hello World!6" << endl;
 }
 
